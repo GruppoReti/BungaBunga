c039c2d46c2152c7cb78adeb292f819ba7168550
gphilo <garollafilippo@outlook.com>
2016-06-13 13:21:33 +0200

    modifica di Seba

--- BungaBunga/BungaBunga/Program.cs
+++ BungaBunga/BungaBunga/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BungaBunga
 {
@@ -10,9 +6,9 @@ namespace BungaBunga
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("update");
-            Console.Write("grattini su salvini");
             Console.ReadKey();
+            Console.WriteLine("update");
+            Console.WriteLine("modifica di Seba")
         }
     }
 }
