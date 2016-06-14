d5821a9f962eb3ec512f1b61e77771b3d04976d5
gphilo <garollafilippo@outlook.com>
2016-06-14 17:23:44 +0200

    aggiunta firma e descrizione di massima delle sottofunzioni

--- BungaBunga/BungaBunga/Program.cs
+++ BungaBunga/BungaBunga/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BungaBunga
 {
-    class Program
+    class Program : Iestrometti, Ibungabunga, Iintroduci
     {
         private List<Politico> ListaPolitici = new List<Politico>();
         private List<Escort> ListaEscort = new List<Escort>();
@@ -16,19 +16,59 @@ namespace BungaBunga
 
         }
 
-        private void introduci(string nome,char sesso,int denaro,int età,int altezza,int peso,float colorecapelli,float costituzione,string presenze)
+
+
+        public void introduci(string nome,char sesso,int denaro,int età,int altezza,int peso,float colorecapelli,float costituzione,string presenze)
         {
 
         }
 
-        private void estrometti(string nome)
+
+
+        public void estrometti(string nome)
         {
 
         }
 
-        private void bungabunga(char giorno, int Naccoppiamenti)
-        {
 
+
+        public void bungabunga(char giorno, int Naccoppiamenti)
+        {
+            List<Tuple<Politico, Escort, float>> ListaDiAffinità = new List<Tuple<Politico, Escort, float>>();
+            //per ogni possibile coppia Politico-Escort calcoliamo la discrepanza secondo le indicazioni del testo, e generiamo una Tupla <Politico, Escort, float> da inserire nella lista
+
+            //ultimata la generazione della lista, la riordiniamo per discrepanza
+
+            //consideriamo solo gli Naccoppiamenti migliori della lista
+
+            //chiamiamo la funzione "GeneraOrgie" per calcolare il numero di gruppetti che si vengono a formare
+
+            //chiamiamo la funzione "TrovaOrgione" per identificare la stanza con più elementi -> ci restituisce una lista/array di 3 interi che rappresentano l'output richiesto
         }
+
+
+
+        private float CalcolaAffinità(Politico P, Escort E)  //restituisce il valore di discrepanza tra le preferenze del politico e le caratteristiche della Escort
+        {
+            float discrepanza=0;
+
+            return discrepanza;
+        }
+
+
+
+        private Tuple<string,string> LeggiIstruzione(int n)  //legge l'istruzione alla riga n-esima nel file di input e la restituisce interpretata nella forma di Tupla <(IDevento), (parametri)>
+        {
+            string istruzione=null;
+            string parametri = null;
+            //gestione lettura da file
+
+            //interpretazione della riga letta (istruzione=.... ; parametri = ..... ; NB: i parametri saranno nello stesso ordine del testo, e separati da " ")
+
+            Tuple<string, string> tupla = new Tuple<string, string>(istruzione, parametri);
+            return tupla;
+        }
+
+
     }
 }
